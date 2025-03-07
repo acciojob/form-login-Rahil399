0081d090c6561437f021437c8ed4b166eb07ab25
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2025-03-07 15:00:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	let firstName = document.querySelector("input[name='fname']").value.trim();
+	let lastName = document.querySelector("input[name='lname']").value.trim();
 
+	if(firstName || lastName){
+	alert(`${firstName} ${lastName}`);
+	}else{
+		alert("Please enter your name.");
+	}	
+
+	document.getElementById("form1").addEventListner("sumbit, getFormvalue");
 }
